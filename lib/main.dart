d48c4cca7a2d1be3fb62b79daa6ba1936da0d6cc
Samit Koyom <samitkoyom@gmail.com>
2023-06-07 16:49:46 +0700

    update moudle from local

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_provider_dio/app.dart';
+
+// firebase message handler
+Future<void> _messageHandler(RemoteMessage message) async {
+  if(kDebugMode){
+    print('background message ${message.notification!.body}');
+  }
+}
 
 void main() async {
   // make sure that all the services are initialized
@@ -9,5 +18,8 @@ void main() async {
   // Intialize firebase
   await Firebase.initializeApp();
 
-  runApp(App());
+  // firebase messaging background handler
+  FirebaseMessaging.onBackgroundMessage(_messageHandler);
+
+  runApp(const App());
 }
--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_provider_dio/providers/bottomnav_provider.dart';
 import 'package:mvvm_provider_dio/services/user_api_service.dart';
@@ -8,6 +9,7 @@ import 'package:mvvm_provider_dio/views/bottomnav/product_screen.dart';
 import 'package:mvvm_provider_dio/views/bottomnav/user_screen.dart';
 import 'package:mvvm_provider_dio/views/home/custom_drawer.dart';
 import 'package:provider/provider.dart';
+import 'package:demomodule/demomodule.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +20,38 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // Setup push notification
+  late FirebaseMessaging messaging;
+
+  void setupNotification(){
+    messaging = FirebaseMessaging.instance;
+    messaging.getToken().then((value) {
+      print('FCM Token: $value');
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      print('message recieved');
+      print(event.notification!.body);
+      showDialog(
+        context: context, 
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text(event.notification!.title!),
+            content: Text(event.notification!.body!),
+            actions: [
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                }, 
+                child: Text('Ok')
+              )
+            ],
+          );
+        }
+      );
+    });
+  }
+
   // bottom navigation screen
   var currentTab = [
     UserScreen(),
@@ -25,11 +59,17 @@ class _HomeScreenState extends State<HomeScreen> {
     MapScreen(),
   ];
 
+  int _num = 100;
+
   @override
   void initState() {
     super.initState();
     // Test Call API
     UserApiService().getUser();
+    setupNotification();
+
+    // Test Module from another project
+    _num = Calculator().addOne(_num);
   }
 
   @override
@@ -42,7 +82,12 @@ class _HomeScreenState extends State<HomeScreen> {
           builder: (context, provider, child){
             return Text(provider.title);
           },
-        )
+        ),
+        actions: [
+          TextButton(
+            onPressed: (){}, 
+            child: Text(_num.toString(), style: TextStyle(color: Colors.white)))
+        ],
       ),
       body: Consumer<BottomNavProvider>(
           builder: (context, provider, child){
